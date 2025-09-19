6378a822b4fe62315a22fc3c852742d1cbecf8b8
Camacho Gonzalez <ACAMAC4@bancodebogota.com.co>
2025-09-19 14:29:24 -0500

    eliminar carpetas

--- src/main/java/com/corporate/utils/Constantes.java
+++
@@ -1,7 +0,0 @@
-package com.corporate.utils;
-
-public class Constantes {
-
-    public static final String rutaExcel="src/test/resources/datos/Datos.xlsx";
-    public static final String rutaExcelOperaciones="src/test/resources/datos/Operaciones.xlsx";
-}
